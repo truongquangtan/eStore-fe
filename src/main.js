829a5b3029505ece16736d5e7c8df40d111677e7
Truong Quang Tan <83901803+truongquangtan@users.noreply.github.com>
2022-12-07 23:07:23 +0700

    title page

--- src/main.js
+++ src/main.js
@@ -7,3 +7,13 @@ import router from '@/router'
 createApp(App)
 .use(router)
 .mount('#app')
+
+/* Default title tag */
+const defaultDocumentTitle = 'Training Project'
+
+/* Set document title from route meta */
+router.afterEach((to) => {
+  document.title = to.meta?.title
+    ? `${to.meta.title}`
+    : defaultDocumentTitle
+})
